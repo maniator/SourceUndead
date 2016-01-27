1520578b73b35715e07537cdd3a368a4664aaf4c
Jordan Alexander <jordan.alexander@mace>
2016-01-27 00:13:08 -0500

    Ssube calculated angle for me
    I integrated his functions into mu socket routing methods
    
    Thanks ssube

--- routes/io-functions.js
+++ routes/io-functions.js
@@ -35,16 +35,20 @@ export function move(data, socket, bucket) {
 	client.smembers("game-1", (err, reply) => {
 		reply.map(x => {
 		client.hgetall(x, (err, reply) => {
-				similarTile(reply, socket);
 				const msg = `${reply.user} is on ${reply.x}, ${reply.y}`;
 				io.sockets.emit("locations", {location:msg});
+				if (reply.id != socket.session.player.id) {
+					const tile = similarTile(reply, socket);
+					let prox;
+					if (!tile) prox = proximity(reply, socket);
+					console.log(prox);
+				}
 			});
 		});
 	});
 
 	//string to return to client for event log
 	const string = `${socket.session.player.user} has moved ${data.direction} to [${socket.session.player.x},${socket.session.player.y}]`;
-	console.log(string);
 	io.sockets.emit("somethingelse", {msg:string});
 }
 
@@ -56,10 +60,48 @@ export function init(socket) {
 
 function similarTile(data, socket) {
 	if (data.user == socket.session.player.user) return false;
-	console.log(`${socket.session.player.x},${socket.session.player.y} (${socket.session.player.user}), ${data.x},${data.y} (${data.user})`);
-	console.log(typeof +data.x, typeof +data.y, typeof socket.session.player.x, typeof socket.session.player.y);
 	if (+data.x == socket.session.player.x && +data.y == socket.session.player.y) {
 		const string = `${socket.session.player.user} and ${data.user} are on the same tile!`;
 		io.sockets.emit("sameTile", {msg:string});
+		return true;
+	} else return false;
+}
+
+function proximity(data, socket) {
+	// Get the distance as a unit vector
+	const v = getDistance(data, socket.session.player);
+	console.log("Distance to player", v)
+	const rad = withinRadius(v, socket.session.player.radius);
+	console.log(rad)
+	if (rad) {
+		const uv = makeUnit(v);
+		const angle = angleFromAtan(Math.atan2(+uv.y, +uv.x));
+		return angle;
+	} else {
+		return -1;
+	}
+}
+
+function withinRadius(v, r) {
+	console.log(v.l, typeof v.l, r)
+	return v.l <= r;
+}
+
+function getDistance(playerA, playerB) {
+	const x = playerA.x - playerB.x;
+	const y = playerA.y - playerB.y;
+	const l = Math.sqrt((x * x) + (y * y));
+	return {x, y, l};
+}
+
+function makeUnit(v) {
+	return {x: (+v.x / +v.l), y: (+v.y / +v.l), l: 1};
+}
+
+function angleFromAtan(a) {
+	if (a > 0) {
+		return (a * 360 / (2 * Math.PI));
+	} else {
+		return ((2*Math.PI + a) * 360 / (2*Math.PI));
 	}
 }
